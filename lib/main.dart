030b292b32aa0354d37e9c8f9e6c23e521e02b82
ZaidShaikh <szaid5146@gmail.com>
2024-10-30 15:39:13 +0530

    Camera feature added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
 import 'package:image_picker/image_picker.dart';
 
 void main() {
@@ -37,35 +38,60 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   File? image;
   late ImagePicker imagePicker;
+  late ImageLabeler labeler;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     imagePicker = ImagePicker();
+    ImageLabelerOptions options = ImageLabelerOptions(confidenceThreshold: 0.5);
+    labeler = ImageLabeler(options: options);
   }
 
   chooseImage() async {
-    XFile? selectedImage = await imagePicker.pickImage(
-        source: ImageSource.gallery);
+    XFile? selectedImage =
+        await imagePicker.pickImage(source: ImageSource.gallery);
     if (selectedImage != null) {
       image = File(selectedImage.path);
+      performImageLabeling();
       setState(() {
         image;
       });
     }
   }
-    captureImage() async {
-      XFile? selectedImage = await imagePicker.pickImage(
-          source: ImageSource.camera);
-      if (selectedImage != null) {
-        image = File(selectedImage.path);
-        setState(() {
-          image;
-        });
-      }
+
+  captureImage() async {
+    XFile? selectedImage =
+        await imagePicker.pickImage(source: ImageSource.camera);
+    if (selectedImage != null) {
+      image = File(selectedImage.path);
+      performImageLabeling();
+      setState(() {
+        image;
+      });
+    }
+  }
+
+  String results = "";
+  performImageLabeling() async {
+    results = "";
+    InputImage inputImage = InputImage.fromFile(image!);
+
+    final List<ImageLabel> labels = await labeler.processImage(inputImage);
+
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final int index = label.index;
+      final double confidence = label.confidence;
+      print(text + "  " + confidence.toString());
+      results += text + " " + confidence.toStringAsFixed(2) + "\n";
     }
 
+    setState(() {
+      results;
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -77,25 +103,70 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Colors.limeAccent.shade400,
+        title: Text("Plant Detection"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            image == null
-                ? const Icon(
-                    Icons.image,
-                    size: 150,
-                  )
-                : Image.file(image!),
-            ElevatedButton(onPressed: () {
-              chooseImage();
-            },onLongPress: (){
-              captureImage();
-            }, child: Text("Choose/Capture"))
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Card(
+                color: Colors.grey,
+                margin: EdgeInsets.all(10),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2,
+                  child: image == null
+                      ? const Icon(
+                          Icons.image,
+                          size: 150,
+                        )
+                      : Image.file(image!),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(10),
+                color: Colors.limeAccent.shade400,
+                child: Container(
+                  height: 100,
+                  child: Row(
+                    children: [
+                      InkWell(
+                        child: Icon(
+                          Icons.image_search,
+                          size: 50,
+                        ),
+                        onTap: () {
+                          chooseImage();
+                        },
+                      ),
+                      InkWell(
+                        child: Icon(
+                          Icons.camera,
+                          size: 50,
+                        ),
+                        onTap: () {
+                          captureImage();
+                        },
+                      ),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  ),
+                ),
+              ),
+              Card(
+                child: Container(
+                  color: Colors.grey,
+                  child: Text(results,style: TextStyle(fontSize: 24,color: Colors.black),),
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.all(10),
+                ),
+                margin: EdgeInsets.all(10),
+
+              ),
+            ],
+          ),
         ),
       ),
     );
